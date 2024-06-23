d19db97c8c36d182e84cb5159a2e786f429153ab
nicofishman <nicourifishman@gmail.com>
2024-06-23 14:20:01 -0300

    JWT secure

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { UserService } from 'src/user/user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
   async getUserProfile(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.findById(id);
